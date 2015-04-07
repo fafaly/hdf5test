c59f540e5371d16f468cfb06907968d1ecd4bc78
fafaly <fafaly@126.com>
2015-04-07 11:56:18 +0800

    0.9

---
+++ testhdf5/hdf5func.h
@@ -0,0 +1,17 @@
+#pragma once
+//create a dataset
+int CreateDataSet();
+//creat group
+int CreateGroup();
+//Create groups in a file using absolute and relative paths
+int CreateGroupPar();
+//write data
+int WriteData();
+//create a attribute
+int CreateAttribute();
+//Create a file and dataset and select/read a subset from the dataset
+int SubSet();
+//Create a chunked and compressed dataset
+int CreateExtend();
+//Create a chunked and compressed dataset
+int CreateChunk();
