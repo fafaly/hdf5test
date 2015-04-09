70f01d2c66723dc619785be9a04865e0c59c060e
fafaly <fafaly@126.com>
2015-04-09 21:05:15 +0800

    0.91

--- testhdf5/hdf5func.h
+++ testhdf5/hdf5func.h
@@ -1,12 +1,25 @@
 #pragma once
+#include "H5Cpp.h"
+#include <string>
+#include <iostream>
+
+#ifndef H5_NO_NAMESPACE
+using namespace H5;
+using namespace std;
+#ifndef H5_NO_STD
+using std::cout;
+using std::endl;
+#endif  // H5_NO_STD
+#endif
+
 //create a dataset
-int CreateDataSet();
+int CreateDataSet(const char *FILE_NAME, const char* DATASET_NAME);
 //creat group
-int CreateGroup();
+int CreateGroup(const char * FILE_NAME, const char* groupname);
 //Create groups in a file using absolute and relative paths
 int CreateGroupPar();
 //write data
-int WriteData();
+int WriteData(const char* FILE_NAME, const char*	DATASET_NAME);
 //create a attribute
 int CreateAttribute();
 //Create a file and dataset and select/read a subset from the dataset
